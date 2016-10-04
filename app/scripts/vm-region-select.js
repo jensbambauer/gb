9a9e7c6b75c70e615160d089751bc3fe16f81a01
Jens Bambauer <jens.bambauer@sinnerschrader.com>
2016-10-04 14:19:47 +0200

    new build

--- app/scripts/vm-region-select.js
+++ app/scripts/vm-region-select.js
@@ -4,33 +4,33 @@
  * @description
  *
  */
- 
+
 /*jslint browser: true*/
 
 
 (function ($, undefined) {
     'use strict';
-    
+
     function RegionSelect(node) {
         var vm = this;
-        
+
         vm.setRegion = function($data, reg) {
             var layer = overlay.api;
-            
+
             ko.cleanNode(layer.getOverlay()[0]);
-            
+
             overlay.api.close();
 
             localStorage.setItem('region', $data);
-            
+
             $('body').addClass('currency-' + $data);
-            
+
             $(document).trigger('regionUpdate');
         };
     }
-    
+
     function openRegionSelect() {
-        $.get('/data/region-select.html', function(data) {
+        $.get(config.assetsPath + '/data/region-select.html', function(data) {
             $('body').append('<div class="overlay-contents">'+ data +'</div>');
             var layer = overlay.api;
 
@@ -40,26 +40,23 @@
             ko.applyBindings(new RegionSelect(this), layer.getOverlay()[0]);
         });
     }
-    
+
     $('[data-ko-model="region-select"]').each(function() {
         ko.applyBindings(new RegionSelect(this), this);
     });
-    
+
     $('[data-role="region-select"]').each(function() {
         $(this).on('click', function(e) {
             e.preventDefault();
-            
+
             openRegionSelect();
         });
     });
-    
+
     if(!localStorage.getItem('region')) {
         openRegionSelect();
     } else {
         $('body').addClass('currency-' + localStorage.getItem('region'));
     }
-  
+
 })(jQuery);
-
-
-            
